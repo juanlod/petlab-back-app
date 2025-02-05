8eac95b82621a3cfbe2e6ced1758a52197aa0516
Juan José López Díaz <31276123+juanlod@users.noreply.github.com>
2025-02-05 20:02:51 +0100

    mongose error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   secret: 'F4DBBEDD9239B919FF24B2D75BCA8',
-  database: 'mongodb+srv://juancorvelle:pe450woVBCtryPZf@cluster0.e1gnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  database: 'mongodb+srv://<juancorvelle>:<pe450woVBCtryPZ>f@cluster0.e1gnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   origin: '*',
 };
